e9b7d132df24ca822284fd23ec747ecd0cccef01
Paindarn <87530192+Paindarn@users.noreply.github.com>
2023-01-19 15:41:01 +0100

    initial commit

--- components/Home.js
+++ components/Home.js
@@ -1,12 +1,28 @@
-import styles from '../styles/Home.module.css';
+import styles from "../styles/Home.module.css";
+import City from "./city";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faSearch } from "@fortawesome/free-solid-svg-icons";
 
 function Home() {
+  const [cities, setCities] = useState([
+    { cityName: "London" },
+    { cityName: "Paris" },
+  ]);
+
   return (
     <div>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <div className={styles.Navbar}>
+          <span className={styles.logo}>Météo RITE</span>
+          <div className={styles.searchInputContainer}>
+            <FontAwesomeIcon icon={faSearch} className={styles.searchIcon} />
+            <input className={styles.searchInput}></input>
+          </div>
+          <FontAwesomeIcon icon={faUser} />
+        </div>
+        <div className={styles.container}>
+          <City></City>
+        </div>
       </main>
     </div>
   );
---
+++ components/city.js
@@ -0,0 +1,13 @@
+import styles from "../styles/city.module.css";
+
+function City() {
+  return (
+    <div>
+      <main className={styles.cardContainer}>
+        <span>card</span>
+      </main>
+    </div>
+  );
+}
+
+export default City;
